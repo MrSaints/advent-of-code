590501d4c2956079c680e9883b9a0f65b8dff095
Ian Lai <MrSaints@users.noreply.github.com>
2015-12-06 04:43:58 +0000

    Add surface_area method to pass test

--- specs/day_02_spec.cr
+++ specs/day_02_spec.cr
@@ -9,6 +9,6 @@ describe "Day_02" do
         day_02 = Day_02.new
         surface_area = day_02.calculate_surface_area "2x3x4"
         surface_area.should eq [+12, +24, +16]
-        surface_area.sum(+0).eq +52
+        surface_area.sum(+0).should eq +52
     end
 end
--- src/day_02.cr
+++ src/day_02.cr
@@ -1,3 +1,6 @@
 class Day_02
-    
+    def calculate_surface_area(lwh : String)
+        l, w, h = lwh.split('x').map { |d| d.to_i }
+        [2 * l * w, 2 * w * h, 2 * h * l]
+    end
 end
