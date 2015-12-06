7e08750f3a14d430fb9f3fd8234e16c714f4a5db
Ian Lai <MrSaints@users.noreply.github.com>
2015-12-06 05:01:52 +0000

    Update paper_needed method to make tests pass

--- src/day_02.cr
+++ src/day_02.cr
@@ -1,14 +1,17 @@
 class Day_02
-    def calculate_surface_area(lwh : String)
+    def calculate_surface_area(lwh : String) : Array
         l, w, h = lwh.split('x').map { |d| d.to_i }
         [2 * l * w, 2 * w * h, 2 * h * l]
     end
 
-    def smallest_side(surface_area : Array)
+    def smallest_side(surface_area : Array) : Number
         surface_area.min / 2
     end
 
-    def paper_needed(lwh : String)
-        0
+    def paper_needed(lwh : String) : Number
+        surface_area = calculate_surface_area lwh
+        extra_paper = smallest_side surface_area
+        total = surface_area.sum(+0) + extra_paper
+        total
     end
 end
