0eb3e22302ec1b492227ee3b006bb65399bca0c5
Ian Lai <MrSaints@users.noreply.github.com>
2015-12-06 22:44:25 +0000

    Refactor to use modules

--- src/day_03.cr
+++ src/day_03.cr
@@ -1,22 +1,24 @@
-struct Santa_GPS
-    getter x, y
+module Day_02
+    struct Santa_GPS
+        getter x, y
 
-    def initialize(@x : Number, @y : Number)
-    end
+        def initialize(@x : Number, @y : Number)
+        end
 
-    def move_north
-        @x += 1
-    end
+        def move_north
+            @x += 1
+        end
 
-    def move_south
-        @x -= 1
-    end
+        def move_south
+            @x -= 1
+        end
 
-    def move_west
-        @y -= 1
-    end
+        def move_west
+            @y -= 1
+        end
 
-    def move_east
-        @y += 1
+        def move_east
+            @y += 1
+        end
     end
 end
