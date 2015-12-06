4dc05dafe247f05a5e8b394f7e355b06a1fc4815
Ian Lai <MrSaints@users.noreply.github.com>
2015-12-06 04:56:15 +0000

    Add method to get smallest side

--- specs/day_02_spec.cr
+++ specs/day_02_spec.cr
@@ -6,9 +6,28 @@ require "../src/day_02"
 
 describe "Day_02" do
     describe "surface_area" do
-        day_02 = Day_02.new
-        surface_area = day_02.calculate_surface_area "2x3x4"
-        surface_area.should eq [+12, +24, +16]
-        surface_area.sum(+0).should eq +52
+        it "should correctly calculate the surface area" do
+            day_02 = Day_02.new
+            surface_area = day_02.calculate_surface_area "2x3x4"
+            surface_area.should eq [+12, +24, +16]
+            surface_area.sum(+0).should eq +52
+
+            surface_area_2 = day_02.calculate_surface_area "1x1x10"
+            surface_area_2.should eq [+2, +20, +20]
+            surface_area_2.sum(+0).should eq +42
+        end
+    end
+
+    describe "smallest_side" do
+        it "should return the value of the smallest side" do
+            day_02 = Day_02.new
+            surface_area = day_02.calculate_surface_area "2x3x4"
+            smallest_side = day_02.smallest_side surface_area
+            smallest_side.should eq 6
+
+            surface_area_2 = day_02.calculate_surface_area "1x1x10"
+            smallest_side_2 = day_02.smallest_side surface_area_2
+            smallest_side_2.should eq 1
+        end
     end
 end
--- src/day_02.cr
+++ src/day_02.cr
@@ -3,4 +3,8 @@ class Day_02
         l, w, h = lwh.split('x').map { |d| d.to_i }
         [2 * l * w, 2 * w * h, 2 * h * l]
     end
+
+    def smallest_side(surface_area : Array)
+        surface_area.min / 2
+    end
 end
