1b77047599e537c4900ef9b3c5682ff9962e20b0
Ian Lai <MrSaints@users.noreply.github.com>
2015-12-06 06:25:47 +0000

    Major refactoring

--- specs/day_02_spec.cr
+++ specs/day_02_spec.cr
@@ -4,47 +4,88 @@ require "../src/day_02"
 # Given a line separated list of lxwxh dimensions
 # How many total square feet of wrapping paper is needed?
 
-describe "Day_02" do
+describe "Paper" do
+    describe "initialize" do
+        it "should set the length, width, and height to +1" do
+            paper = Paper.new +1, +2, +3
+            paper.l.should eq +1
+            paper.w.should eq +2
+            paper.h.should eq +3
+        end
+    end
+
     describe "surface_area" do
-        it "should correctly calculate the surface area using a lxwxh string" do
-            day_02 = Day_02.new
-            surface_area = day_02.calculate_surface_area "2x3x4"
+        it "should correctly calculate the surface area for 2x3x4" do
+            paper = Paper.new +2, +3, +4
+            surface_area = paper.surface_area
             surface_area.should eq [+12, +24, +16]
             surface_area.sum(+0).should eq +52
+        end
 
-            surface_area_2 = day_02.calculate_surface_area "1x1x10"
-            surface_area_2.should eq [+2, +20, +20]
-            surface_area_2.sum(+0).should eq +42
+        it "should correctly calculate the surface area for 1x1x10" do
+            paper = Paper.new +1, +1, +10
+            surface_area = paper.surface_area
+            surface_area.should eq [+2, +20, +20]
+            surface_area.sum(+0).should eq +42
         end
     end
 
     describe "smallest_side" do
-        it "should return the value of the smallest side using a surface area array" do
-            day_02 = Day_02.new
-            surface_area = day_02.calculate_surface_area "2x3x4"
-            smallest_side = day_02.smallest_side surface_area
-            smallest_side.should eq +6
+        it "should correctly return the area of the smallest side for 2x3x4" do
+            paper = Paper.new +2, +3, +4
+            paper.smallest_side.should eq +6
+        end
 
-            surface_area_2 = day_02.calculate_surface_area "1x1x10"
-            smallest_side_2 = day_02.smallest_side surface_area_2
-            smallest_side_2.should eq +1
+        it "should correctly return the area of the smallest side for 1x1x10" do
+            paper = Paper.new +1, +1, +10
+            paper.smallest_side.should eq +1
+        end
+    end
+
+    describe "total" do
+        it "should correctly return the total square feet of paper needed for 2x3x4" do
+            paper = Paper.new +2, +3, +4
+            paper.total.should eq +58
+        end
+
+        it "should correctly return the total square feet of paper needed for 1x1x10" do
+            paper = Paper.new +1, +1, +43
+        end
+    end
+end
+
+describe "Day_02" do
+    describe "initialize" do
+        it "should normalize a single lxwxh present" do
+            day_02 = Day_02.new "2x3x4"
+            day_02.presents.should eq [[+2, +3, +4]]
+        end
+
+        it "should normalize a line separated list of lxwxh presents" do
+            day_02 = Day_02.new "2x3x4\n1x1x10"
+            day_02.presents.should eq [[+2, +3, +4], [+1, +1, +10]]
+        end
+    end
+
+    describe "convert_to_array" do
+        it "should convert a lxwxh string to an array" do
+            day_02 = Day_02.new ""
+            day_02.convert_to_array("2x3x4").should eq [+2, +3, +4]
         end
     end
 
     describe "paper_needed" do
-        it "should return the total square feet of paper needed for a present" do
-            day_02 = Day_02.new
-            day_02.paper_needed("2x3x4").should eq +58
-            day_02.paper_needed("1x1x10").should eq +43
+        it "should return the total square feet of paper needed for all presents" do
+            day_02 = Day_02.new "2x3x4\n1x1x10"
+            day_02.paper_needed.should eq +101
         end
     end
 
-    describe "process" do
-        it "should return the total square feet of paper needed for all presents" do
-            test_data = "2x3x4\n1x1x10"
-            day_02 = Day_02.new
-            total_needed = day_02.process test_data
-            total_needed.should eq 101
+    describe "answers" do
+        it "part 01 should be +1606483" do
+            presents = File.read("./data/day_02.txt")
+            day_02 = Day_02.new presents
+            day_02.paper_needed.should eq +1606483
         end
     end
 end
--- src/day_02.cr
+++ src/day_02.cr
@@ -1,26 +1,42 @@
 # TODO: Refactor (add presents to class? per present methods?)
+struct Paper
+    getter l, w, h
+
+    def initialize(@l : Number, @w : Number, @h : Number)
+    end
+
+    def surface_area : Array(Int32)
+        [+2 * @l * @w, +2 * @w * @h, +2 * @h * @l]
+    end
+
+    def smallest_side : Number
+        surface_area.min / +2
+    end
+
+    def total : Number
+        surface_area.sum + smallest_side
+    end
+end
+
 class Day_02
-    def calculate_surface_area(lwh : String) : Array
-        l, w, h = lwh.split('x').map { |d| d.to_i }
-        [2 * l * w, 2 * w * h, 2 * h * l]
-    end
+    getter presents
 
-    def smallest_side(surface_area : Array) : Number
-        surface_area.min / 2
-    end
-
-    def paper_needed(lwh : String) : Number
-        surface_area = calculate_surface_area lwh
-        extra_paper = smallest_side surface_area
-        total = surface_area.sum(+0) + extra_paper
-        total
-    end
-
-    def process(presents : String) : Number
-        total = 0
+    def initialize(presents : String)
+        @presents = [] of Array(Int32 | Int32 | Int32)
         presents.each_line do |present|
-          total += paper_needed present
+            @presents << convert_to_array present
         end
-        total
+    end
+
+    def convert_to_array(lwh : String) : Array(Int32)
+        lwh.split('x').map { |d| d.to_i }
+    end
+
+    def paper_needed : Number
+        @presents.sum do |present|
+            l, w, h = present
+            paper = Paper.new l, w, h
+            paper.total
+        end
     end
 end
