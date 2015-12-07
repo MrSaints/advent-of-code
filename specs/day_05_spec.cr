c19b8667fb152916db6f5e0c0739a119983019cb
Ian Lai <MrSaints@users.noreply.github.com>
2015-12-07 19:45:20 +0000

    Test scaffolding for Day 05

---
+++ specs/day_05_spec.cr
@@ -0,0 +1,27 @@
+require "spec"
+require "../src/day_05"
+
+# Given a string containing random alphabets
+# Determine if it's a "nice" string:
+# 1. Contains 3 vowels ("aeiou"), e.g. xazegov
+# 2. Contains 1 letter that appears twice in a row
+# 3. Does not contain ab, cd, pq, or xy
+
+describe "Day_05" do
+    describe "NaughtyOrNice" do
+        describe "initialize" do
+        end
+
+        describe "has_vowels" do
+        end
+
+        describe "has_consecutive_letter"
+        end
+
+        describe "does_not_contain"
+        end
+
+        describe "is_nice"
+        end
+    end
+end
